88bd3ac5527eeee69c12a9671f53f26281ba22a4
Jeremy Davis <jeremy@turnkeylinux.org>
2019-07-16 13:24:54 +1000

    add gitignore and setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+from setuptools import setup
+
+setup(
+    name='turnkey-gitwrapper',
+    version='0.9',
+    description="Py3 wrapper library for working with git directories",
+    author='TurnKey GNU/Linux',
+    author_email='jeremy@turnkeylinux.org',
+    license='GPLv3+',
+    url='https://github.com/JedMeister/turnkey-gitwrapper',
+    py_modules=['git',],
+)
