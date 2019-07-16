878a70b0672570b4f2579a402517f91720b78e39
Jeremy Davis <jeremy@turnkeylinux.org>
2019-07-16 13:59:35 +1000

    rename git.py to gitwrapper.py

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ from setuptools import setup
 
 setup(
     name='turnkey-gitwrapper',
-    version='0.9',
+    version='0.10',
     description="Py3 wrapper library for working with git directories",
     author='TurnKey GNU/Linux',
     author_email='jeremy@turnkeylinux.org',
     license='GPLv3+',
     url='https://github.com/JedMeister/turnkey-gitwrapper',
-    py_modules=['git',],
+    py_modules=['gitwrapper',],
 )
