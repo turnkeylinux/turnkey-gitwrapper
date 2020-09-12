09017de4b2e2de862c1b1ce7b9f4b646acbe79c4
Jeremy Davis <jeremy@turnkeylinux.org>
2020-09-12 21:57:44 +1000

    Bump version.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='turnkey-gitwrapper',
-    version='0.11',
+    version='0.12',
     description="Py3 wrapper library for working with git directories",
     author='TurnKey GNU/Linux',
     author_email='jeremy@turnkeylinux.org',
